2bf98c2f6124531c45aebbc4a77774a0036cfd38
ruben <caibugatti.ruben@franchettisalviani.net>
2026-04-14 10:58:43 +0200

    Grafica
    Ho preparato la grafica che mi servirà per far andare la corsa

---
+++ src/threadrelay/GestoreGara.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package threadrelay;
+
+/**
+ *
+ * @author caibugatti.ruben
+ */
+public class GestoreGara {
+    FormGara gara=new FormGara(this);
+    
+    public void show(){
+        gara.setVisible(true);
+    }
+}
--- src/threadrelay/ThreadRelay.java
+++ src/threadrelay/ThreadRelay.java
@@ -14,7 +14,9 @@ public class ThreadRelay {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GestoreGara gestore=new GestoreGara();
+        
+        gestore.show();
     }
     
 }
